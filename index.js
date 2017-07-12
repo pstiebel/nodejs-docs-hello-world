caad2847f996d8b6e884ad0601ab11281d71612d
pstiebel <pstiebel@users.noreply.github.com>
2017-07-12 15:31:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello to the World outside!");
+    response.end("Hello to the World outside again!");
 
 });
 
